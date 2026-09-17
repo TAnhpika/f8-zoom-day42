a56b2a5d22ae07f5ba4b16c35afc0663c53a950e
Anhpika <anhpika12345@gmail.com>
2026-09-17 15:25:46 +0700

    loading & disabled

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,11 +3,14 @@ import clsx from "clsx";
 
 import styles from "./Button.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 function Button({
     primary = false,
     rounded = false,
     bordered = false,
+    loading = false,
+    disabled = false,
     children,
     href,
     size = "medium",
@@ -15,22 +18,57 @@ function Button({
     icon,
     leftIcon = icon,
     rightIcon,
+    onClick,
     ...passProps
 }) {
+    const shouldDisabled = disabled || loading;
     const classNames = clsx(styles.wrapper, styles[size], className, {
         [styles.primary]: primary,
         [styles.rounded]: rounded,
         [styles.bordered]: bordered,
+        [styles.disabled]: shouldDisabled,
     });
 
     const Component = href ? "a" : "button";
 
+    const handleClick = (e) => {
+        // chống dev user ẩn css rồi submit
+        if (shouldDisabled) {
+            e.preventDefault();
+            e.stopPropagation();
+            return;
+        }
+        onClick(e);
+    };
+
     return (
         // để ...passProps trc để k ghi đè className, logic ,... quan trọng phía sau
-        <Component {...passProps} href={href} className={clsx(classNames)}>
-            {leftIcon && <FontAwesomeIcon className={styles.icon} icon={leftIcon} />}
-            <span>{children}</span>
-            {rightIcon && <FontAwesomeIcon className={styles.icon} icon={rightIcon} />}
+        <Component
+            {...passProps}
+            onClick={handleClick}
+            href={href}
+            className={clsx(classNames)}
+        >
+            <div
+                className={clsx(styles.inner, {
+                    [styles.hidden]: loading,
+                })}
+            >
+                {leftIcon && (
+                    <FontAwesomeIcon className={styles.icon} icon={leftIcon} />
+                )}
+                <span>{children}</span>
+                {rightIcon && (
+                    <FontAwesomeIcon className={styles.icon} icon={rightIcon} />
+                )}
+            </div>
+            {loading && (
+                <FontAwesomeIcon
+                    className={styles.loading}
+                    icon={faSpinner}
+                    spin
+                />
+            )}
         </Component>
     );
 }
@@ -41,11 +79,14 @@ Button.propTypes = {
     primary: PropTypes.bool,
     rounded: PropTypes.bool,
     bordered: PropTypes.bool,
+    disabled: PropTypes.bool,
+    loading: PropTypes.bool,
     href: PropTypes.string,
     size: PropTypes.string,
     className: PropTypes.string,
     leftIcon: PropTypes.object,
     rightIcon: PropTypes.object,
+    onClick: PropTypes.func,
 };
 
 export default Button;
--- src/pages/Icons/index.jsx
+++ src/pages/Icons/index.jsx
@@ -1,14 +1,30 @@
 import Button from "@/components/Button";
-import { faChevronLeft, faChevronRight, faLink } from "@fortawesome/free-solid-svg-icons";
+import {
+    faChevronLeft,
+    faChevronRight,
+    faLink,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Icons() {
+    let loading = true;
+
     return (
         <div>
             <h1>Icons</h1>
-            <Button icon={faLink}>Button 1</Button>
-            <Button leftIcon={faLink}>Button 2</Button>
+            <Button
+                icon={faLink}
+                loading={loading}
+                onClick={() => alert("Clicked!")}
+            >
+                Button 1
+            </Button>
+            <Button leftIcon={faLink} disabled>
+                Button 2
+            </Button>
             <Button rightIcon={faChevronRight}>Button 3</Button>
-            <Button leftIcon={faChevronLeft} rightIcon={faChevronRight}>Button 4</Button>
+            <Button leftIcon={faChevronLeft} rightIcon={faChevronRight}>
+                Button 4
+            </Button>
         </div>
     );
 }
