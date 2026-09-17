5ae3e79c48af209529636837cf2e3404fc7e99b8
Anhpika <anhpika12345@gmail.com>
2026-09-17 13:41:37 +0700

    fontawesome loading & animation control

--- src/pages/Icons/index.jsx
+++ src/pages/Icons/index.jsx
@@ -1,21 +1,32 @@
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
-import { faHeart, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+    faHeart,
+    faMagnifyingGlass,
+    faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Icons() {
+    // Fake
+    let loading = true;
+
     return (
         <div>
             <h1>Icons</h1>
             <button
                 style={{
                     fontSize: 20,
-                    color: "blue",
+                    color: "red",
+                    "--fa-animation-duration": "1s",
                 }}
             >
+                <FontAwesomeIcon
+                    spin={loading}
+                    icon={loading ? faSpinner : faGoogle}
+                />
                 Click me!
             </button>
             <FontAwesomeIcon icon={faMagnifyingGlass} />
-            <FontAwesomeIcon icon={faGoogle} />
 
             <FontAwesomeIcon icon={faHeart} beat />
         </div>
