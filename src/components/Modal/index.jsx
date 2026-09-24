466cc27fbef6ae3bad164a9ebce9d5d5215d49b8
Anhpika <anhpika12345@gmail.com>
2026-09-24 13:14:44 +0700

    Clear modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -6,22 +6,18 @@ import clsx from "clsx";
 const Modal = forwardRef(
     (
         {
-            // đổi tên thành _isOpen trong component nhưng bên ngoài vẫn truyền prop isOpen bth
             isOpen: _isOpen = false,
             shouldCloseOnOverlayClick = true,
             shouldCloseOnEsc = true,
             children,
-            // closeTimeoutMS: giúp hỗ trợ modal animation
             closeTimeoutMS = 0,
             className,
-            // bodyOpenClassName: ẩn thanh scrollbar khi mở modal
             bodyOpenClassName,
             overlayClassName,
             onRequestClose,
         },
         ref,
     ) => {
-        // khi re-render isOpen k dùng lại giá trị khởi tạo là _isOpen nữa
         const [isOpen, setIsOpen] = useState(_isOpen);
 
         useEffect(() => {
@@ -49,27 +45,10 @@ const Modal = forwardRef(
             setTimeout(onRequestClose, closeTimeoutMS);
         };
 
-        // useEffect cần đứng trc điều kiện return vì
-        //  khi components đc sử dụng thì hooks phải lun đc chạy
         useEffect(() => {
-            // vđ: khi modal chưa bật vẫn lắng nghe sự kiện
-            // vđ: k remove đc
-            // lý do: mỗi lần chạy lại sẽ tạo ra 1 context mới -> trong context mới đó tạo lại hàm handle mới
-            // muốn remove thì cần truyền cùng 1 tham chiếu
-            // load home -> Modal đc mounted -> đã gọi callback -> tạo handle ở #0x001
-            // -> isOpen = false -> remove #0x001
-            // khi click open modal, components vẫn đang mounted
-            // open modal -> render lại home -> render lại Modal
-            // -> callback chạy lại vì isOpen đổi -> tạo lại hàm handle #0x002
-            // -> addEventListener #0x002
-            // khi click đóng modal -> Home re-render -> Modal re-render -> tạo handle #0x003
-            // -> remove #0x003 khi #0x002 vẫn đang lắng nghe
-
             if (!shouldCloseOnEsc) return;
 
             const handle = (e) => {
-                // console.log(e.code);
-
                 if (e.code === "Escape") {
                     handleRequestClose();
                 }
@@ -79,12 +58,8 @@ const Modal = forwardRef(
                 document.addEventListener("keyup", handle);
             }
 
-            // console.log("Callback");
-
             // Cleanup function
             return () => {
-                // console.log("Cleanup");
-
                 document.removeEventListener("keyup", handle);
             };
         }, [shouldCloseOnEsc, isOpen, onRequestClose, handleRequestClose]);
@@ -96,7 +71,6 @@ const Modal = forwardRef(
                 document.body.classList.remove(bodyOpenClassName);
             };
         }, [bodyOpenClassName]);
-        // console.log("Render");
 
         if (!isOpen) return null;
 
