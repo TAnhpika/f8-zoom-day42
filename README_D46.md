65190eeab2c689d03e791add3f45fab1ca017927
Anhpika <anhpika12345@gmail.com>
2026-09-26 16:42:22 +0700

    Infinite Scroll Component

--- README_D46.md
+++ README_D46.md
@@ -3,6 +3,7 @@
 - persist cực kỳ hữu ích. nên hiểu và áp dụng (state đang ntn, F5 giữ nguyên toàn bộ). Như khi lưu config setting, lưu form đang nhập dở...
 
 ## Fix lỗi gọi product api 2 lần & persist 'me' data
+
 - add authSlice vào blacklist
 - thêm authPersistConfig, đưa fetching vào blacklist (để k bắn 2 lần)
 - để hiện lun thông tin 'me' luôn thay vì nhấp nháy nút log in r hiện
@@ -11,5 +12,9 @@
 ## Mã hóa dữ liệu lưu localStorage = persist encrypt
 
 - npm i redux-persist-transform-encrypt
-- dùng thêm btoa trick lỏ. Cách chính bên BE yêu cầu trình duyệt lưu dạng httpOnlyCookie - k set/get đc = JS (tránh XSS). 
+- dùng thêm btoa trick lỏ. Cách chính bên BE yêu cầu trình duyệt lưu dạng httpOnlyCookie - k set/get đc = JS (tránh XSS).
 - Còn cái gì dính đến tiền mới áp dụng thêm (k tự mã hóa chính mình)
+
+# Infinite Scroll Component
+
+npm i react-infinite-scroll-component
--- src/pages/ProductList/index.jsx
+++ src/pages/ProductList/index.jsx
@@ -1,57 +1,42 @@
 import { useProductLoading, useProductList } from "@/features/product";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { useInView } from "react-intersection-observer";
+import { useDispatch, useSelector } from "react-redux";
 import { getList } from "@/services/product";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function ProductList() {
     const dispatch = useDispatch();
     const [page, setPage] = useState(1);
-    const [hasMore, setHasMore] = useState(true);
-
-    const { ref, inView } = useInView({
-        threshold: 0,
-        rootMargin: "200px",
-    });
 
     const products = useProductList();
     const loading = useProductLoading();
-
-    useEffect(() => {
-        if (!hasMore || loading) return;
-        if (!inView) return;
-        // eslint-disable-next-line react-hooks/set-state-in-effect
-        setPage((prev) => prev + 1);
-    }, [hasMore, inView, loading]);
+    const hasMore = useSelector((state) => state.product.hasMore);
 
     useEffect(() => {
         dispatch(getList({ limit: 10, page }));
     }, [dispatch, page]);
 
-    useEffect(() => {
-        if (!loading && products.length > 0 && products.length % 10 !== 0) {
-            // eslint-disable-next-line react-hooks/set-state-in-effect
-            setHasMore(false);
-        }
-    }, [loading, products]);
-
     return (
         <>
             <h1>Product List</h1>
 
-            <ul>
-                {products.map((product) => (
-                    <li key={product.id}>{product.title}</li>
-                ))}
-            </ul>
-            {loading && <div>Loading...</div>}
-
-            {hasMore && (
-                <div ref={ref} style={{ textAlign: "center", padding: "20px" }}>
-                    {loading ? "Đang tải..." : "Cuộn để xem thêm"}
-                </div>
-            )}
-            {!hasMore && <p>Đã hết dữ liệu</p>}
+            <InfiniteScroll
+                dataLength={products.length}
+                next={() => {
+                    if (!loading && hasMore) {
+                        setPage((currentPage) => currentPage + 1);
+                    }
+                }}
+                hasMore={hasMore}
+                loader={<div>Đang tải...</div>}
+                endMessage={<p>Đã hết danh sách</p>}
+            >
+                <ul>
+                    {products.map((product) => (
+                        <li key={product.id}>{product.title}</li>
+                    ))}
+                </ul>
+            </InfiniteScroll>
         </>
     );
 }
